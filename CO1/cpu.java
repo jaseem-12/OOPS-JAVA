d2b0df7e52646450756936a0e6790aa8e6104535
JASEEM C <121373690+jaseem-12@users.noreply.github.com>
2023-06-21 12:07:50 +0530

    Create cpu.java

---
+++ CO1/cpu.java
@@ -0,0 +1,35 @@
+public class cpu{
+    int price;
+     class processor{
+        int cores;
+        String producer;
+        processor(int noC, String manu){
+            cores=noC;
+            producer=manu;
+        }
+        void display(){
+        System.out.println("\nProcessor info");
+        System.out.println("No. of Cores = "+cores);
+        System.out.println("Manufacturer = "+producer+"\n");
+    }
+    }
+    static class ram{
+        int mem;
+        String manuf;
+        ram(int memory,String producer ){
+            mem=memory;
+            manuf=producer;
+        }
+        void display(){
+        System.out.println("\nRAM info");
+        System.out.println("Memory = "+mem+" GB");
+        System.out.println("Manufacturer = "+manuf+"\n");
+    }}
+    public static void main(String[] args) {
+         cpu.ram obj1= new cpu.ram(8,"Intel");
+         cpu.processor obj2 = new cpu.processor(8,"Samsung");
+         obj1.display();
+         obj2.display();
+
+    }
+}
